ee801776704a8b431dda4a825276a58a5f6b6207
Andy Dawson <andydawson76@gmail.com>
2020-03-27 09:09:39 +0100

    Update version tags

--- src/DDTrace/version.php
+++ src/DDTrace/version.php
@@ -2,4 +2,4 @@
 
 // Must begin with a number for Debian packaging requirements
 // Must use single-quotes for packaging script to work
-return '0.41.1'; // Update Tracer::VERSION too
+return '0.41.1.sg1'; // Update Tracer::VERSION too
